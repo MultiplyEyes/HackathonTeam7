733077a9fabd1973d3c936533bfbe546f345a558
sybren loos <sybre.loosn@hva.com>
2025-03-25 13:07:40 +0100

    grape

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,28 @@
-const state = {
+const state = { 
     inline: {
-      change: undefined,
-      changing: undefined,
+        change: undefined,
+        changing: undefined,
     },
     block: {
-      change: undefined,
-      changing: undefined,
+        change: undefined,
+        changing: undefined,
     }
-  }
-  
-  demo.onscrollsnapchange = event => {
-    state.inline.change?.classList.remove('snapped-inline')
-    state.block.change?.classList.remove('snapped-block')
+};
+
+
+demo.onscrollsnapchanging = function(event) {
+    if (state.block.changing, state.inline.changing) {
+        state.block.changing.classList.remove('snap-changing-inline');
+        state.inline.changing.classList.remove('snap-changing-inline');
+    }
+
+    if (event.snapTargetInline) {
+        event.snapTargetInline.classList.add('snap-changing-inline');
+    }
+    if (event.snapTargetBlock) {
+        event.snapTargetBlock.classList.add('snap-changing-inline');
+    }
     
-    event.snapTargetInline?.classList.add('snapped-inline')
-    event.snapTargetBlock?.classList.add('snapped-block')
-    
-    state.inline.change = event.snapTargetInline
-    state.block.change = event.snapTargetBlock
-  }
-  
-  demo.onscrollsnapchanging = event => {
-    state.inline.changing?.classList.remove('snap-changing-inline')
-    state.block.changing?.classList.remove('snap-changing-block')
-    
-    event.snapTargetInline?.classList.add('snap-changing-inline')
-    event.snapTargetBlock?.classList.add('snap-changing-block')
-    
-    state.inline.changing = event.snapTargetInline
-    state.block.changing = event.snapTargetBlock
-  }
+    state.inline.changing = event.snapTargetInline;
+    state.block.changing = event.snapTargetBlock;
+};

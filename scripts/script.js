552546f3d17823582b6b77c726d8ef54ed2d4654
sybren loos <sybre.loosn@hva.com>
2025-03-26 14:03:34 +0100

    more

--- scripts/script.js
+++ scripts/script.js
@@ -31,7 +31,7 @@ const loadMoreItems = (count = 10) => {
     const fragment = document.createDocumentFragment();
     shuffledWomen.forEach(woman => {
         let womenHTML = `
-                        <li>
+                        <li >
                             <a href=persoon.html?id=${woman.id}>
                                 <ul class="card" style="background-image: url('https://fdnd.directus.app/assets/${woman.image}');">
                                     <li class="name">
@@ -87,5 +87,5 @@ const observeListItems = () => {
 };
 
 // Initial load with more items
-loadMoreItems(20);
+loadMoreItems(50);
 observeListItems();

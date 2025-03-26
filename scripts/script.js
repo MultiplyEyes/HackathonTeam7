29063833ef0346d6e73d9f91386c7a2431b4ebc9
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2025-03-26 14:00:54 +0100

    Details and styling

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ const loadMoreItems = (count = 10) => {
                             <a href=persoon.html?id=${woman.id}>
                                 <ul class="card" style="background-image: url('https://fdnd.directus.app/assets/${woman.image}');">
                                     <li class="name">
-                                        <h1>${woman.name}</h1>
+                                        <h2>${woman.name}</h2>
                                     </li>
                                     <li class="color">
                                     </li>
